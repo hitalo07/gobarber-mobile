fb925d7b11ef40b8e97be42766fdbf1b227a60ba
hitalo07 <hitalocunhadesousa@gmail.com>
2019-11-14 16:41:23 -0300

    chore: Config Deploy

--- src/routes.js
+++ src/routes.js
@@ -60,7 +60,7 @@ export default (isSigned = false) =>
           {
             resetOnBlur: true,
             tabBarOptions: {
-              keyboardHhidesTabBar: true,
+              keyboardHidesTabBar: true,
               activeTintColor: '#fff',
               inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
               style: {
